5990a33a9f6d769e1badacb9f3d15ec2ae1c1244
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2020-01-30 00:44:56 -0700

    routing and data coming through

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import {Container, Dimmer, Loader,} from 'semantic-ui-react';
+import Home from './components/Home';
+import People from './components/People';
+import Planets from './components/Planets';
+
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+    useEffect(() => {
+      async function fetchPeople() {
+        let res = await fetch('https://swapi.co/api/people/?format=json');
+        let data = await res.json();
+        setPeople(data.results);
+      }
+
+      async function fetchPlanets() {
+        let res  = await fetch('https://swapi.co/api/people/?format=json');
+        let data = await res.json();
+        setPlanets(data.results);
+      }
+
+      fetchPeople();
+      fetchPlanets();
+    },[])    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <>
+     <Router>
+       <Navbar />
+       <Container>
+         <Switch>
+           <Route exact path='/' >
+             <Home />
+           </Route>
+           <Route exact path='/people'>
+              <People />
+           </Route>
+           <Route exact path='/planets' >
+              <Planets />
+           </Route>
+         </Switch>
+       </Container>
+       </Router>
+     </>
     </div>
   );
 }
